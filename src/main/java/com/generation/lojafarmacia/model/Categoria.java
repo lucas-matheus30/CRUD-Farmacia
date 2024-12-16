4e662d6863df23fec8e7d45a6551c17fb41b78f1
Lucas Matheus <lucas-matheusdev@outlook.com>
2024-12-16 11:57:45 -0300

    construção do crud da entidade Categoria

---
+++ src/main/java/com/generation/lojafarmacia/model/Categoria.java
@@ -0,0 +1,86 @@
+package com.generation.lojafarmacia.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	private String nome;
+	
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	private String descricao;
+	
+	@NotBlank(message = "O atributo tipo é obrigatório!")
+	private String tipo;
+	
+	@NotBlank(message = "O atributo status é obrigatório!")
+	private String status;
+	
+	@UpdateTimestamp
+	private LocalDateTime dataCriacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+	
+}
---
+++ src/main/java/com/generation/lojafarmacia/model/Produto.java
@@ -0,0 +1,5 @@
+package com.generation.lojafarmacia.model;
+
+public class Produto {
+
+}
