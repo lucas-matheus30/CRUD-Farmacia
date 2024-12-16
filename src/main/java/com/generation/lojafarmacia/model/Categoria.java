3309fecd21b2463dfc9645312e76ca6f4988f238
Lucas Matheus <lucas-matheusdev@outlook.com>
2024-12-16 14:18:35 -0300

    construção do crud da entidade produto

--- src/main/java/com/generation/lojafarmacia/model/Categoria.java
+++ src/main/java/com/generation/lojafarmacia/model/Categoria.java
@@ -1,38 +1,48 @@
 package com.generation.lojafarmacia.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tb_categorias")
 public class Categoria {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank(message = "O atributo nome é obrigatório!")
 	private String nome;
-	
+
 	@NotBlank(message = "O atributo descrição é obrigatório!")
 	private String descricao;
-	
+
 	@NotBlank(message = "O atributo tipo é obrigatório!")
 	private String tipo;
-	
+
 	@NotBlank(message = "O atributo status é obrigatório!")
 	private String status;
-	
+
 	@UpdateTimestamp
 	private LocalDateTime dataCriacao;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -81,6 +91,13 @@ public class Categoria {
 	public void setDataCriacao(LocalDateTime dataCriacao) {
 		this.dataCriacao = dataCriacao;
 	}
-	
-	
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
 }
--- src/main/java/com/generation/lojafarmacia/model/Produto.java
+++ src/main/java/com/generation/lojafarmacia/model/Produto.java
@@ -1,5 +1,113 @@
 package com.generation.lojafarmacia.model;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+@Entity
+@Table(name = "tb_produtos")
 public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	private String nome;
+
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	private String descricao;
+
+	@NotNull(message = "O preço não pode ser nulo!")
+	@PositiveOrZero(message = "O preço não pode ser negativo")
+	private Double preco;
+
+	@NotNull(message = "O estoque não pode ser nulo!")
+	@PositiveOrZero(message = "O estoque não pode ser negativo")
+	private Integer qtdEstoque;
+
+	@NotNull(message = "A data de validade não pode ser nula!")
+	private LocalDate dataValidade;
+
+	@NotBlank(message = "O atributo status é obrigatório!")
+	private String status;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQtdEstoque() {
+		return qtdEstoque;
+	}
+
+	public void setQtdEstoque(Integer qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}
+
+	public LocalDate getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(LocalDate dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
 }
